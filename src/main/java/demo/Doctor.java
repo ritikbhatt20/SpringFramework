6dd1df11dc3147236f531eeafd338f3d77947fa8
ritik <ritikbhatt020@gmail.com>
2023-10-24 02:44:27 +0530

    Learned about beans in Spring

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,8 +1,18 @@
 package demo;
 
-public class Doctor {
+public class Doctor implements Staff{
+
+    private String qualification;
 
     public void assist(){
         System.out.println("Doctor is assisting");
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
     }
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,9 +1,15 @@
 package demo;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
-        doctor.assist();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+//        Doctor doctor = context.getBean(Doctor.class);
+        Staff staff =  context.getBean(Doctor.class);
+//        doctor.assist();
+        staff.assist();
     }
 }
