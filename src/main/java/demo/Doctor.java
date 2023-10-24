5165e1dc39d616a37a1129bc0802dc1834577637
ritik <ritikbhatt020@gmail.com>
2023-10-24 17:23:52 +0530

    Annotation and Java based configuration done... POstconstruct, setBeanName

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,13 +1,29 @@
 package demo;
 
-public class Doctor implements Staff{
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
-//    properties
-    private String qualification;
+import javax.annotation.PostConstruct;
 
-    public Doctor(String qualification) {
-        this.qualification = qualification;
+@Component
+@Scope(scopeName = "prototype")
+//means whatever the class we have created is the component that you can use to create the beans out of it
+public class Doctor implements Staff, BeanNameAware {
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "qualification='" + qualification + '\'' +
+                '}';
     }
+
+    //    properties
+    private String qualification;
+//
+//    public Doctor(String qualification) {
+//        this.qualification = qualification;
+//    }
 //    adding the object itself
 
     public void assist(){
@@ -20,5 +36,17 @@ public class Doctor implements Staff{
 
     public void setQualification(String qualification) {
         this.qualification = qualification;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("Set Bean name method is called");
+    }
+
+//    once the object is created, at that time if you want to call anything
+//    Custom method implementation for bean lifecycle
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Post Construct method is called");
     }
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,16 +1,23 @@
 package demo;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 
 //        Doctor doctor = context.getBean(Doctor.class);
-        Doctor staff =  context.getBean(Doctor.class);
-//        doctor.assist();
-        staff.assist();
-        System.out.println(staff.getQualification());
+        Doctor doctor =  context.getBean(Doctor.class);
+        doctor.assist();
+        doctor.setQualification("MBBS");
+//        System.out.println(staff.getQualification());
+//        System.out.println(doctor);
+//
+//        Doctor doctor1 = context.getBean(Doctor.class);
+//        System.out.println(doctor1);
     }
 }
